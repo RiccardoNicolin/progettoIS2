0036d8884b83c33721d6d6019bd748366521e301
Riccardo Nicolin <riccardo.nicolin@studenti.unitn.it>
2020-11-12 16:24:49 +0100

    giusto

--- DB.js
+++ DB.js
@@ -6,7 +6,7 @@ const dati = {
 
 const lista_utenti = {
     insert (utente){
-        dati.lista_utenti.push({nome: utente.useraome, email: utente.email, password: utente.password});
+        dati.lista_utenti.push({nome: utente.username, email: utente.email, password: utente.password});
         return;
     },
     cercaPerMail(email){
