a898f189d2030058b1e7a9e010109348320f8eee
unknown <leonardo.masera@studenti.unitn.it>
2020-11-23 15:21:37 +0100

    removed console logs

--- DB.js
+++ DB.js
@@ -59,20 +59,15 @@ const lista_serie = {
 
     modificaCategoria(id, target, modifica){
    let serie = this.cercaPerNome(id);
-   console.log(serie);
    serie[target] = modifica;
-   console.log(serie);
     },
 
     modificaVoto(id, vote){
         let serie = this.cercaPerNome(id);
-        console.log(serie);
         storage = serie.totale;
         storage = +storage + +vote;
         serie.totale = storage;
-        console.log(serie.totale);
         serie.numerovoti = serie.numerovoti + 1;
-        console.log(serie.numerovoti);
         serie.voto = (serie.totale / serie.numerovoti).toFixed(2);
     },
 
