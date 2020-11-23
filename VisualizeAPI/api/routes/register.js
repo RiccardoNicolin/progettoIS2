d65f7287cdcdbfe9021dce15824753d899ed4b87
unknown <leonardo.masera@studenti.unitn.it>
2020-11-23 16:13:58 +0100

    fixed logic v3

--- VisualizeAPI/api/routes/register.js
+++ VisualizeAPI/api/routes/register.js
@@ -7,7 +7,7 @@ var bcrypt = require('bcrypt');
 // User signup
 router.post('/', (req, res, next) =>{
 // Check if user submitted all fields
-if (!req.body.email | !req.body.username | !req.body.password){
+if (!req.body.email || !req.body.username || !req.body.password){
   res.status(500).json({message: "Username, Email and Password are mandatory"});
 }
 else {
@@ -15,7 +15,7 @@ else {
   var foundemail = db.lista_utenti.cercaPerMail(req.body.email);
   var foundusername = db.lista_utenti.cercaPerNome(req.body.username);
   //if it doesn't exist then create new user
-  if (!foundemail & !foundusername){
+  if (!foundemail && !foundusername){
     //hashing the password, salting it 10fold and checking if hash was successfull
     bcrypt.hash(req.body.password, 10, function (err, hashedpass) {
       if (err) {
