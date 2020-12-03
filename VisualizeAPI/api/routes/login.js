69fd876484cfb70f455bb83bbfca75684e848e06
unknown <leonardo.masera@studenti.unitn.it>
2020-12-03 18:25:51 +0100

    beginning user validation vote still broken tho

--- VisualizeAPI/api/routes/login.js
+++ VisualizeAPI/api/routes/login.js
@@ -26,7 +26,7 @@ router.post('/', async (req, res) =>{
                     username: user.username,
                     admin: user.admin
                 }, process.env.JWT_KEY, {
-                    expiresIn: "1s"
+                    expiresIn: "1h"
                 } )
                 return res.status(200).json({
                     message: "Authentication successful",
