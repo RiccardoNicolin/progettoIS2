183c7bb7a6faf4c609bd449648fcf661eb6be5b1
unknown <leonardo.masera@studenti.unitn.it>
2020-12-03 15:29:02 +0100

    changed token verification with "000" exception

--- VisualizeAPI/api/routes/home.js
+++ VisualizeAPI/api/routes/home.js
@@ -5,38 +5,43 @@ const serie = require('../../../DB/serie');
 const jwt = require("jsonwebtoken");
 
 
-router.get('/', async (req, res) =>{
+router.get('/', async (req, res) => {
     //get req for home page, visualizes hot and new series for unsubscribed user
     try {
         //trying to look for token, if token is present and is valid also search the users bookmarked/in vision series
         const token = req.headers.authorization.split(" ")[1];
-        //const check = jwt.verify(token, process.env.JWT_KEY);
-       // req.verifydec = verifydec;
+        if (token != "000") {
+            const check = jwt.verify(token, process.env.JWT_KEY);
+        }
+        else {
+            throw "Missing Token";
+        }
 
-    } catch (error){
+    } catch (error) {
         //if error businness as usual it's not logged in
-            let serieshot = await serie.findMore('tag', "hot") //seleziona tutte le serie dove uno degli elementi del campo tag è quello specificato
-            let seriesnew = await serie.findMore('tag', "new");
-            res.status(200).json({
-                serieshot,
-                seriesnew,
-                verifydec: "" //pass invalid decoded token
-                });
-        } finally{
-            //TODO when merged put series bookmarked/in vision
-            let serieshot = await serie.findMore('tag', "hot") //seleziona tutte le serie dove uno degli elementi del campo tag è quello specificato
-            let seriesnew = await serie.findMore('tag', "new");
-            //let token = req.headers.authorization.split(" ")[1];
-            //let verifydec = jwt.verify(token, process.env.JWT_KEY);
-            res.status(200).json({
-                serieshot,
-                seriesnew,
-                //verifydec: verifydec//pass decoded token
-                });
-            }
+        let serieshot = await serie.findMore('tag', "hot") //seleziona tutte le serie dove uno degli elementi del campo tag è quello specificato
+        let seriesnew = await serie.findMore('tag', "new");
+        res.status(200).json({
+            serieshot,
+            seriesnew,
+            verifydec: "" //pass invalid decoded token
+        });
+    }
+
+    //TODO when merged put series bookmarked/in vision
+    let serieshot = await serie.findMore('tag', "hot") //seleziona tutte le serie dove uno degli elementi del campo tag è quello specificato
+    let seriesnew = await serie.findMore('tag', "new");
+    let token = req.headers.authorization.split(" ")[1];
+    let verifydec = jwt.verify(token, process.env.JWT_KEY);
+    res.status(200).json({
+        serieshot,
+        seriesnew,
+        verifydec: verifydec //pass decoded token
     });
 
-router.get('/userlist', async (req, res) => { 
+});
+
+router.get('/userlist', async (req, res) => {
     let userlist = await user.getAll();
     //userlist shown
     res.status(200).json(userlist);
