f0d2a5ba867193f0a529c1be66dca7ea252a730a
Ettore <ettore@onecarb.it>
2020-12-08 16:25:31 +0100

    inserito home test corrett

--- test/home.test.js
+++ test/home.test.js
@@ -6,30 +6,46 @@ const request = require("supertest");
 const app = require("../VisualizeAPI/app.js");
 const server="/home/";
 const serie = require("../DB/serie.js");
-const inizializer = require("../DB/MongoDB.js");
+//const inizializer = require("../DB/MongoDB.js");
 
 describe("Test on codes on home/", () =>{
 
     beforeAll(() => {
-        inizializer.inizializeDB();
+        //inizializer.inizializeDB();
     });
 
     test("It should response the GET method affirmatively and return the item", async () => {
         const response = await request(app)
             .get(server)
-            .set('Accept', 'application/json');
+            .set('Accept', 'application/json' )
+            .set({Authorization: 'Bearer 000'});
         expect(response.type).toBe("application/json");
+
+        console.log(response.body);
+
+        let res=response.body;
         
+        let requested = {
+            serieshot: res.serieshot,
+            seriesnew: res.seriesnew
+        };
+        
+        requested=JSON.stringify(requested);
+
+        let expectiong = {
+            serieshot: await serie.findMore('tag', "hot"),
+            seriesnew: await serie.findMore('tag', 'new')
+        };
+
+        expectiong=JSON.stringify(expectiong);
         //assolutamente da cambiare, qui il check è fatto manualmente, se aggiungiamo serie in hot o new crea errore in test
-        expect(response.body).toStrictEqual([
-            serie.findMore('tag', "hot"),
-            serie.findMore('tag', 'new')
-        ]);
+        expect(requested).toBe( expectiong );
     });
 
     test("It should response the GET method positively", async() =>{
         const response = await request(app)
             .get(server)
+            .set('Authorization', 'Bearer 000');
         expect(response.statusCode).toBe(200);
     });
 
