5762f52dd84f82d488aa06a13eace9ba8767eb28
Ettore <ettore@onecarb.it>
2020-12-09 17:48:11 +0100

    morgan

--- VisualizeAPI/app.js
+++ VisualizeAPI/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 //const db = require('../DB.js');
-
+const morgan = require('morgan');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static('public'));
+app.use(morgan('dev'));
 
 const homeRoutes = require('./api/routes/home');
 const seriesRoutes = require('./api/routes/series');
