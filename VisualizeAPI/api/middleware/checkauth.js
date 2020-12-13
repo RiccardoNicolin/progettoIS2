bac65628c60c15200ee5ee99d93b1f4623d551c3
Riccardo Nicolin <riccardo.nicolin@studenti.unitn.it>
2020-12-13 17:13:04 +0100

    add console log for travis

--- VisualizeAPI/api/middleware/checkauth.js
+++ VisualizeAPI/api/middleware/checkauth.js
@@ -4,6 +4,7 @@ module.exports = (req, res, next ) => {
     const token = req.headers.authorization.split(" ")[1];
     if (token != "000"){
         try {
+            console.log(token);
             //checking if the token is valid            
                 const verifydec = jwt.verify(token, process.env.JWT_KEY);
                 req.body.verifydec = verifydec;
